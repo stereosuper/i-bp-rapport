70f8dc657fc9a536d334ebf2f5e809ecfbb43a2c
adrienlementheour <adrien.le.mentheour@gmail.com>
2019-06-06 16:54:33 +0200

    💄 footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,17 @@
 <template>
-    <footer />
+    <footer>
+        <div>
+            ©2019, informatique banque populaire
+        </div>
+        <div>
+            <a class="logo-groupe-bpce" href="https://groupebpce.com/"
+                ><img src="img/logo-groupe-bpce.svg" alt="Groupe BPCE"
+            /></a>
+        </div>
+        <div>
+            <nuxt-link to="/mentions-legales">Mentions légales</nuxt-link>
+        </div>
+    </footer>
 </template>
 
 <script>
@@ -16,4 +28,31 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+footer {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 30px $gutter;
+    font-family: $chivo;
+    font-size: 1.1rem;
+    text-transform: uppercase;
+    color: $primary-light;
+    > div {
+        display: flex;
+        justify-content: flex-start;
+        width: 33.3333%;
+        &:nth-child(2) {
+            justify-content: center;
+        }
+        &:nth-child(3) {
+            justify-content: flex-end;
+        }
+    }
+}
+.logo-groupe-bpce {
+    display: block;
+    width: 100%;
+    max-width: 148px;
+}
+</style>
