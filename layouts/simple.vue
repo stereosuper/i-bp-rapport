c2b057e0213c32c3adbcb7c6010539d65dfc268c
Elisabeth Hamel <elisabeth@stereosuper.fr>
2019-06-13 18:31:14 +0200

    :art: mentions

---
+++ layouts/simple.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Header />
+        <nuxt />
+        <Svgs />
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from "~/components/Header";
+import Footer from "~/components/Footer";
+import Svgs from "~/components/Svgs";
+
+export default {
+    components: {
+        Header,
+        Footer,
+        Svgs
+    }
+};
+</script>
+
+<style></style>
---
+++ pages/mentions/index.vue
@@ -0,0 +1,100 @@
+<template>
+    <div class='container'>
+        <h2>Mentions légales</h2>
+        
+        <h3>Adresses</h3>
+        
+        <h4>Siège social</h4>
+        <p>
+            Le Futura <br>
+            Zac du Pas du Lac <br>
+            23 place de Wicklow <br>
+            78067 Saint-Quentin-en-Yvelines cedex <br>
+            Tél. : 01 30 13 45 00
+        </p>
+
+        <h4>Autres sites</h4>
+        <p>
+            1 rue du Paquier <br>
+            BP 22 <br>
+            21601 Longvic cedex <br>
+            Tél. : 03 80 68 73 73
+        </p>
+
+        <p>
+            56 route de Lavaur <br>
+            BP 63106 <br>
+            31131 Balma cedex <br>
+            Tél. : 05 61 61 71 71
+        </p>
+
+        <p>
+            1 rue Édith Piaf <br>
+            BP 80229 <br>
+            44815 Saint-Herblain cedex <br>
+            Tél. : 02 40 67 48 00
+        </p>
+
+        <p>
+            15 rue Fernand Braudel <br>
+            3e étage <br>
+            75013 Paris <br>
+            Tél. : 01 84 95 15 07
+        </p>
+
+        <p>
+            26 avenue de la Mirande <br>
+            66240 Saint Estève <br>
+            Tél. : 05 61 61 71 71
+        </p>
+
+        <h3>Rapport d'activité 2018 i-BP</h3>
+        <ul>
+            <li><strong>Directeur de publication</strong> : Serge Matry <br></li>
+            <li><strong>Responsable de la publication</strong> : Annick Bellaigue (02 40 67 48 41) <br></li>
+            <li><strong>Comité de rédaction</strong> : Cécile Joret avec la contribution d’Isabelle Tylek,
+            du service communication et des collaborateurs et directions d’i-BP <br></li>
+            <li><strong>Conception et réalisation</strong> : L’Unique Équipe en collaboration avec Les Indiens et Stéréosuper <br></li>
+            <li><strong>Crédits photos</strong> : iStock, Shutterstock, 123rf, Groupe BPCE, Service communication i-BP</li>
+        </ul>
+
+        <h3>Suivre i-BP</h3>
+        <ul>
+            <li><a href='https://www.linkedin.com/company/'>Informatique Banque Populaire</a></li>
+            <li><a href='https://twitter.com/informatiquebp?lang=fr'>@InformatiqueBP</a></li>
+            <li><a href='https://informatique.banquepopulaire.fr/'>www.informatique.banquepopulaire.fr</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    layout: 'simple',
+}
+</script>
+
+<style lang="scss" scoped>
+.container{
+    max-width: 950px;
+    padding: 0 30px 120px;
+    margin: 0 auto;
+}
+
+h2{
+    font-size: 6rem;
+}
+
+p{
+    margin: 2em 0;
+}
+
+li{
+    margin: 0 0 10px;
+}
+
+a{
+    &:hover, &:focus{
+        color: $primary;
+    }
+}
+</style>
