fe61a1762e7e5f8ac8e1e4f92b9332848262edb6
adrienlementheour <adrien.le.mentheour@gmail.com>
2019-06-14 19:11:54 +0200

    📱 filters responsive

--- components/Filters.vue
+++ components/Filters.vue
@@ -9,34 +9,49 @@
             :class="[{ 'is-fixed': isFixed }]"
             ref="filters"
         >
-            <button type="button" class="on">
-                <span class="filter">Toutes</span>
-                <span class="nb-elem">22</span>
-            </button>
-            <button type="button">
-                <span class="filter">#Inside</span>
-                <span class="nb-elem">8</span>
-            </button>
-            <button type="button">
-                <span class="filter">#Performances</span>
-                <span class="nb-elem">7</span>
-            </button>
-            <button type="button">
-                <span class="filter">#Banque</span>
-                <span class="nb-elem">8</span>
-            </button>
-            <button type="button">
-                <span class="filter">#Événement</span>
-                <span class="nb-elem">7</span>
-            </button>
-            <button type="button">
-                <span class="filter">#Transformation</span>
-                <span class="nb-elem">8</span>
-            </button>
-            <button type="button">
-                <span class="filter">#Tendances</span>
-                <span class="nb-elem">7</span>
-            </button>
+            <div class="filter-title">Filtre :</div>
+            <div class="wrapper-button">
+                <button type="button" class="btn-filter">
+                    <svg class="icon icon-filter">
+                        <use xlink:href="#icon-filter"></use>
+                    </svg>
+                </button>
+                <button type="button" class="btn-close">
+                    <svg class="icon icon-cross">
+                        <use xlink:href="#icon-cross"></use>
+                    </svg>
+                </button>
+            </div>
+            <div class="content-filters">
+                <button type="button" class="on">
+                    <span class="filter">Toutes</span>
+                    <span class="nb-elem">22</span>
+                </button>
+                <button type="button">
+                    <span class="filter">#Inside</span>
+                    <span class="nb-elem">8</span>
+                </button>
+                <button type="button">
+                    <span class="filter">#Performances</span>
+                    <span class="nb-elem">7</span>
+                </button>
+                <button type="button">
+                    <span class="filter">#Banque</span>
+                    <span class="nb-elem">8</span>
+                </button>
+                <button type="button">
+                    <span class="filter">#Événement</span>
+                    <span class="nb-elem">7</span>
+                </button>
+                <button type="button">
+                    <span class="filter">#Transformation</span>
+                    <span class="nb-elem">8</span>
+                </button>
+                <button type="button">
+                    <span class="filter">#Tendances</span>
+                    <span class="nb-elem">7</span>
+                </button>
+            </div>
         </div>
     </div>
 </template>
@@ -99,10 +114,21 @@ export default {
     background: $white;
 }
 .filters {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
     position: absolute;
     top: 0;
     left: 0;
     right: 0;
+    background: $white;
+    z-index: 2;
+    &.is-fixed {
+        position: fixed;
+        box-shadow: 0px 0px 30px rgba(50, 51, 94, 0.18);
+    }
+}
+.content-filters {
     display: flex;
     align-self: flex-end;
     width: 100%;
@@ -110,12 +136,6 @@ export default {
     flex-wrap: wrap;
     padding-top: 15px;
     padding-bottom: 15px;
-    background: $white;
-    z-index: 2;
-    &.is-fixed {
-        position: fixed;
-        box-shadow: 0px 0px 30px rgba(50, 51, 94, 0.18);
-    }
     button {
         position: relative;
         font-family: $chivo;
@@ -149,5 +169,55 @@ export default {
     font-size: 1.4rem;
     vertical-align: super;
     color: $secondary;
+}
+.filter-title {
+    display: none;
+    font-family: $chivo;
+    font-size: 1.8rem;
+    color: $primary;
+}
+.wrapper-button {
+    display: none;
+    position: relative;
+    width: 30px;
+    height: 30px;
+    > button {
+        width: 100%;
+        height: 100%;
+        > svg {
+            width: 100%;
+            height: 100%;
+        }
+    }
+    svg {
+        fill: $primary;
+    }
+}
+.btn-close {
+    display: none;
+    padding: 5px;
+}
+
+@media (max-width: $desktop) {
+    .filters {
+        height: 70px;
+    }
+    .filter-title {
+        display: block;
+    }
+    .wrapper-button {
+        display: block;
+    }
+    .content-filters {
+        flex-direction: column;
+        align-items: flex-start;
+        justify-content: flex-start;
+        position: absolute;
+        top: 100%;
+        left: 0;
+        right: 0;
+        padding: 50px $gutter-small;
+        background: $white;
+    }
 }
 </style>
