f577c429239266bf2a37d1061df5fcdc982ae8e2
adrienlementheour <adrien.le.mentheour@gmail.com>
2019-06-18 22:28:50 +0200

    🖼️💄 favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,8 +8,7 @@ export default {
      */
     head: {
         title: pkg.name,
-        meta: [
-            {
+        meta: [{
                 charset: "utf-8"
             },
             {
@@ -20,13 +19,41 @@ export default {
                 hid: "description",
                 name: "description",
                 content: pkg.description
+            },
+            {
+                name: 'msapplication-TileColor',
+                content: '#da532c'
+            },
+            {
+                name: 'theme-color',
+                content: '#ffffff'
             }
         ],
-        link: [
+        link: [{
+                rel: 'apple-touch-icon',
+                sizes: '180x180',
+                href: '/apple-touch-icon.png'
+            },
             {
-                rel: "icon",
-                type: "image/x-icon",
-                href: "/favicon.ico"
+                rel: 'icon',
+                type: 'image/png',
+                sizes: '32x32',
+                href: '/favicon-32x32.png'
+            },
+            {
+                rel: 'icon',
+                type: 'image/png',
+                sizes: '16x16',
+                href: '/favicon-16x16.png'
+            },
+            {
+                rel: 'manifest',
+                href: '/site.webmanifest'
+            },
+            {
+                rel: 'mask-icon',
+                href: '/safari-pinned-tab.svg',
+                color: '#5bbad5'
             }
         ]
     },
